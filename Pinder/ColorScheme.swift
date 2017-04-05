84d59f61dd060698e5aca8026557b7273e1a014c
Kate Harline <kharline@wustl.edu>
2017-04-04 23:40:13 -0500

    added color scheme UIColor extension...these are the colors to use in the app

---
+++ Pinder/ColorScheme.swift
@@ -0,0 +1,23 @@
+//
+//  ColorScheme.swift
+//  Pinder
+//
+//  Created by Kate Harline on 4/4/17.
+//  Copyright © 2017 Kate Harline. All rights reserved.
+//
+
+//these are our current colors in the palette
+
+import UIKit
+
+extension UIColor {
+    
+    static let pinderBlue: UIColor = UIColor(red: 8 / 255, green: 82 / 255, blue: 255 / 255, alpha: 1)
+    static let pinderPurple: UIColor = UIColor(red: 150 / 255, green: 18 / 255, blue: 232 / 255, alpha: 1)
+    static let pinderRed: UIColor = UIColor(red: 255 / 255, green: 0, blue: 0, alpha: 1)
+    static let pinderYellow: UIColor = UIColor(red: 255 / 255, green: 229 / 255, blue: 59 / 255, alpha: 1)
+    static let pinderGreen: UIColor = UIColor(red: 74 / 255, green: 1, blue: 72 / 255, alpha: 1)
+    static let pinderCoral: UIColor = UIColor(red: 255 / 255, green: 99 / 255, blue: 126 / 255, alpha: 1)
+    static let pinderOrange: UIColor = UIColor(red: 255 / 255, green: 142 / 255, blue: 0, alpha: 1)
+    
+}
