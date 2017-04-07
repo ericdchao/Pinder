0681e4485422d0f153e8680a18839aa92e377761
Labuser <Labuser@wtk316-18.cec.wustl.edu>
2017-04-07 15:52:06 -0500

    Added DatabaseFunctions.swift

--- Pinder/Pinder/ViewController.swift
+++ Pinder/Pinder/ViewController.swift
@@ -7,10 +7,16 @@
 //
 
 import UIKit
+import Firebase
+
+var ref: FIRDatabaseReference! = FIRDatabase.database().reference()
 
 class ViewController: UIViewController {
 
+
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
