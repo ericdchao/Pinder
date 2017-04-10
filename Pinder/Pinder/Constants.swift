8ac25445375c664aa8cae72d1504a76790903530
Labuser <Labuser@wtk316-30.cec.wustl.edu>
2017-04-10 14:37:51 -0500

    added login functions to database

---
+++ Pinder/Pinder/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Pinder
+//
+//  Created by Labuser on 4/10/17.
+//  Copyright © 2017 Kate Harline. All rights reserved.
+//
+
+import Foundation
--- Pinder/Pinder/DatabaseFunctions.swift
+++ Pinder/Pinder/DatabaseFunctions.swift
@@ -36,5 +36,32 @@ func saveNewPet(username: String, password: String){
 func changeUserProfile(username : String, dictionary: Dictionary<String, String>){
     let userRef = ref.child("users").child(username)
     userRef.updateChildValues(dictionary)
-    
+
 }
+
+func login(username: String, password: String) -> Int {
+    let newRef = ref.child("users");
+    var ans = userPassDoesNotExist;
+    newRef.observeSingleEvent(of: .value, with: { snapshot in
+        // do some stuff once
+        if(!snapshot.exists()){
+            ans =  -1; //Error!!! Somebody messed up our database schema for users
+        } else if(snapshot.hasChild("/(username)")){
+            if(newRef.child("/username").value(forKey: "password") as! String == password){
+                ans = isUser;
+            }
+        }
+        
+    });
+    ref.child("pets").observeSingleEvent(of: .value, with: {snapshot in
+        if(snapshot.hasChild("/(username)")){
+            if(ref.child("pets").child("/(username)").value(forKey: "password") as! String == password){
+                ans = isPet;
+            }
+        }
+});
+
+    return ans;
+}
+
+
