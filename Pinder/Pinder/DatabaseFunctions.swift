c6e471c8b0e6309d18cbe67d83c39e949cc41e1c
Labuser <Labuser@wtk316-18.cec.wustl.edu>
2017-04-10 17:58:21 -0500

    Added Profile functions for users and pets

--- Pinder/Pinder/DatabaseFunctions.swift
+++ Pinder/Pinder/DatabaseFunctions.swift
@@ -12,7 +12,6 @@ import Firebase
 
 //New User Functions
 func saveNewUser(username: String, password: String) {
-    
     let newUserRef = ref.child("users")
     newUserRef.child(username.lowercased())
     let newChildUserRef = newUserRef.child(username.lowercased())
@@ -25,7 +24,6 @@ func changeUserName(oldUsername: String, newUsername: String){
 }
 
 func saveNewPet(username: String, password: String){
-    
     ref.child("pets").child(username).setValue(["username" : username,
                                                 "password": password])
     
@@ -33,10 +31,9 @@ func saveNewPet(username: String, password: String){
 
 //https://www.firebase.com/docs/ios/guide/saving-data.html
 
-func changeUserProfile(username : String, dictionary: Dictionary<String, String>){
-    let userRef = ref.child("users").child(username)
+func changeUserProfile(username : String, userType : String, dictionary: Dictionary<String, userProfileElement>){
+    let userRef = ref.child(userType).child(username).child("profile")
     userRef.updateChildValues(dictionary)
-
 }
 
 func login(username: String, password: String) -> Int {
@@ -64,4 +61,20 @@ func login(username: String, password: String) -> Int {
     return ans;
 }
 
+//userType = users if people, pets if not
+func retrieveUserProfile(username: String, userType : String) -> Dictionary<String,userProfileElement>{
+    var dictionary = Dictionary<String,userProfileElement>()
 
+    ref.child(userType).child("/(username)").child("profile").observeSingleEvent(of: .value, with: { (snapshot) in
+        // Get user value
+        dictionary = (snapshot.value as? Dictionary<String,userProfileElement>)!
+    }) { (error) in
+        print(error.localizedDescription)
+    }
+    return dictionary
+}
+
+protocol userProfileElement {}
+extension String : userProfileElement {}
+extension UIImage : userProfileElement {}
+extension Double : userProfileElement {}
