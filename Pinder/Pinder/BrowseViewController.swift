82f4486a52235a4b50cd17291f38c9d8e74da2b3
Guanyu Chen <decrecyc@gmail.com>
2017-04-11 13:45:10 -0500

    swipe

--- Pinder/Pinder/BrowseViewController.swift
+++ Pinder/Pinder/BrowseViewController.swift
@@ -18,12 +18,65 @@ class BrowseViewController: UIViewController {
         performSegue(withIdentifier: "toSettings", sender: nil)
     }
     
+    @IBOutlet weak var petImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.wasDragged(gestureRecognizer:)))
+        
+        petImage.isUserInteractionEnabled = true
+        
+        petImage.addGestureRecognizer(gesture)
+
+        
     }
+    
+    func wasDragged(gestureRecognizer: UIPanGestureRecognizer) {
+        
+        let translation = gestureRecognizer.translation(in: view)
+        
+        let label = gestureRecognizer.view!
+        
+        label.center = CGPoint(x: self.view.bounds.width / 2 + translation.x, y: self.view.bounds.height / 2 + translation.y)
+        
+        let xFromCenter = label.center.x - self.view.bounds.width / 2
+        
+        var rotation = CGAffineTransform(rotationAngle: xFromCenter / 200)
+        
+        let scale = min(abs(100 / xFromCenter), 1)
+        
+        var stretchAndRotation = rotation.scaledBy(x: scale, y: scale) // rotation.scaleBy(x: scale, y: scale) is now rotation.scaledBy(x: scale, y: scale)
+        
+        label.transform = stretchAndRotation
+        
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.ended {
+            
+            if label.center.x < 100 {
+                
+                print("Not chosen")
+                
+            } else if label.center.x > self.view.bounds.width - 100 {
+                
+                print("Chosen")
+                
+            }
+            
+            rotation = CGAffineTransform(rotationAngle: 0)
+            
+            stretchAndRotation = rotation.scaledBy(x: 1, y: 1) // rotation.scaleBy(x: scale, y: scale) is now rotation.scaledBy(x: scale, y: scale)
+            
+            
+            label.transform = stretchAndRotation
+            
+            label.center = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
+            
+        }
+        
+    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
