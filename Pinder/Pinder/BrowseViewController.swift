0ff809ebbf6b9fd189797bc180d5782d93045662
Guanyu Chen <decrecyc@gmail.com>
2017-04-16 23:26:55 -0500

    popup

--- Pinder/Pinder/BrowseViewController.swift
+++ Pinder/Pinder/BrowseViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import CDAlertView
 
 class BrowseViewController: UIViewController {
 
@@ -25,7 +26,9 @@ class BrowseViewController: UIViewController {
         // self.imageView.image = UIImage(nextImage)
     }
     
-    
+    let matchAlert = CDAlertView(title: "It's a Match !", message: "Well explained message!", type: .notification)
+    let doneAction = CDAlertViewAction(title: " Contact her💪")
+    let nevermindAction = CDAlertViewAction(title: "Keep Swiping 😑")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,9 +39,14 @@ class BrowseViewController: UIViewController {
         petImage.isUserInteractionEnabled = true
         
         petImage.addGestureRecognizer(gesture)
+        matchAlert.add(action: nevermindAction)
+        matchAlert.add(action: doneAction)
 
-        
     }
+    
+
+    
+    
     
     func wasDragged(gestureRecognizer: UIPanGestureRecognizer) {
         
@@ -68,14 +76,19 @@ class BrowseViewController: UIViewController {
                 
             } else if label.center.x > self.view.bounds.width - 100 {
                 
-                
-                let label = UILabel(frame: CGRect(x: self.view.bounds.width / 2 - 100, y: self.view.bounds.height / 2 - 50, width: 200, height: 100))
-                
-                label.text = "Drag me!"
-                
-                label.textAlignment = NSTextAlignment.center
-                
-                view.addSubview(label)
+//                
+//                let label = UILabel(frame: CGRect(x: self.view.bounds.width / 2 - 100, y: self.view.bounds.height / 2 - 50, width: 200, height: 100))
+//                
+//                label.text = "Drag me!"
+//                
+//                label.textAlignment = NSTextAlignment.center
+                matchAlert.show()
+//                var contentView:UIView
+//                
+//                convenience init(contentView: UIView)
+//                
+//                view.addSubview(label)
+
                 
                 print("Chosen")
                 updataImage()
